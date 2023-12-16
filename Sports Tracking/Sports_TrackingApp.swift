4356e3fda03ade6d96364d943dfec5d67f60e780
Mirsait Sabirzyanov <mirsait77777@gmail.com>
2023-12-16 22:17:01 +0300

    Initial Commit

---
+++ Sports Tracking/Sports_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sports_TrackingApp.swift
+//  Sports Tracking
+//
+//  Created by Мирсаит Сабирзянов on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Sports_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
